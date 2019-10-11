7456247bbb71b463a9f0d6fcd2afc197155889ce
MagnavoxTG <dominik.lohner92@gmx.at>
2019-10-11 16:14:18 +0200

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/PrimeNumbers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrimeNumbers.dir/main.c.o"
+  "PrimeNumbers"
+  "PrimeNumbers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrimeNumbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
